7b98685e411785522dc132440de947a5ec8bacf6
Luis Galeana <lsgaleana@gmail.com>
2025-07-29 22:42:28 -0400

    Fix port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,22 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  envDir: './client',
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  server: {
-    port: parseInt(process.env.VITE_FRONTEND_PORT),
-    host: '0.0.0.0',
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: `${process.env.VITE_BACKEND_URL}`,
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+    server: {
+      port: parseInt(env.VITE_FRONTEND_PORT),
+      host: '0.0.0.0',
+      strictPort: true,
+      proxy: {
+        '/api': {
+          target: `${env.VITE_BACKEND_URL}`,
+          changeOrigin: true
+        }
       }
     }
   }
