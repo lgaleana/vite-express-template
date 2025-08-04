a5b1e05ca5a4bae9507293666fca76858002db7e
Luis Galeana <lsgaleana@gmail.com>
2025-08-04 16:40:56 -0400

    Revert

--- server/tracing/babel-plugin-function-logger.js
+++ server/tracing/babel-plugin-function-logger.js
@@ -1,23 +1,14 @@
-module.exports = function () {
-  const t = require('@babel/types');
-
+module.exports = function() {
   return {
     visitor: {
       Function(path) {
         const functionName = getFunctionName(path.node);
         if (!functionName) return; // Skip anonymous functions
-
+        
         const fileName = this.file.opts.filename?.split('/').pop() || 'unknown';
-
+        
         // Inject logging into function body
         injectLogging(path, functionName, fileName);
-      },
-
-      CallExpression(path) {
-        if (isExpressRouteCall(path.node)) {
-          const fileName = this.file.opts.filename?.split('/').pop() || 'unknown';
-          instrumentExpressRoute(path, fileName);
-        }
       }
     }
   };
@@ -25,36 +16,37 @@ module.exports = function () {
   function getFunctionName(node) {
     // Function declarations: function foo() {}
     if (node.id && node.id.name) return node.id.name;
-
+    
     // Method definitions: class methods, object methods
     if (node.key && node.key.name) return node.key.name;
-
+    
     // Variable declarations: const foo = function() {}
     const parent = node.parent;
     if (parent && parent.type === 'VariableDeclarator' && parent.id && parent.id.name) {
       return parent.id.name;
     }
-
+    
     // Property assignments: obj.foo = function() {}
     if (parent && parent.type === 'Property' && parent.key && parent.key.name) {
       return parent.key.name;
     }
-
+    
     return null;
   }
-
+  
   function injectLogging(path, functionName, fileName) {
-
+    const t = require('@babel/types');
+    
     if (!path.node.body || path.node.body.type !== 'BlockStatement') {
       // Arrow functions with expression bodies: () => expr
       // Convert to block statement
       const returnStmt = t.returnStatement(path.node.body);
       path.node.body = t.blockStatement([returnStmt]);
     }
-
+    
     const isAsync = path.node.async;
     const originalStatements = [...path.node.body.body];
-
+    
     // Entry log: ENTER|FUNCTION|file||name|args
     const entryLog = t.expressionStatement(
       t.callExpression(
@@ -69,7 +61,7 @@ module.exports = function () {
         ])]
       )
     );
-
+    
     // Exit log: EXIT|FUNCTION|file||name|result
     const createExitLog = (resultVar) => t.expressionStatement(
       t.callExpression(
@@ -82,7 +74,7 @@ module.exports = function () {
         ])]
       )
     );
-
+    
     // Error log: ERROR|FUNCTION|file||name|error
     const errorLog = t.expressionStatement(
       t.callExpression(
@@ -95,7 +87,7 @@ module.exports = function () {
         ])]
       )
     );
-
+    
     if (isAsync) {
       // Async function handling
       const wrappedBody = [
@@ -149,110 +141,5 @@ module.exports = function () {
       ];
       path.node.body = t.blockStatement(wrappedBody);
     }
-  }
-
-  // Express route detection and instrumentation
-  function isExpressRouteCall(node) {
-
-    if (!t.isMemberExpression(node.callee)) return false;
-
-    const methodName = t.isIdentifier(node.callee.property) ? node.callee.property.name : null;
-    const receiver = node.callee.object;
-
-    const EXPRESS_METHODS = ['get', 'post', 'put', 'delete'];
-
-    return !!(methodName && EXPRESS_METHODS.includes(methodName) &&
-      t.isIdentifier(receiver) &&
-      (receiver.name === 'app' || receiver.name === 'router'));
-  }
-
-  function extractRoutePath(node) {
-    const firstArg = node.arguments[0];
-
-    if (t.isStringLiteral(firstArg)) {
-      return firstArg.value;
-    }
-    return '/*';
-  }
-
-  function instrumentExpressRoute(path, fileName) {
-    const node = path.node;
-
-    const methodName = t.isIdentifier(node.callee.property) ? node.callee.property.name : null;
-    const routePath = extractRoutePath(node);
-
-    if (!methodName) return;
-
-    // Instrument handler functions in the arguments
-    const instrumentedArgs = node.arguments.map(arg => {
-      if (t.isFunctionExpression(arg) || t.isArrowFunctionExpression(arg)) {
-        return addEndpointLogging(arg, methodName.toUpperCase(), routePath, fileName);
-      }
-      return arg;
-    });
-
-    // Update the call expression with instrumented handlers
-    path.replaceWith(
-      t.callExpression(node.callee, instrumentedArgs)
-    );
-  }
-
-  function addEndpointLogging(handler, method, routePath, fileName) {
-
-    // Create endpoint log: ENTER|ENDPOINT|METHOD|PATH|FILENAME|req.body
-    const endpointLog = t.expressionStatement(
-      t.callExpression(
-        t.memberExpression(t.identifier('console'), t.identifier('log')),
-        [t.templateLiteral([
-          t.templateElement({ raw: `ENTER|ENDPOINT|${method}|${routePath}|${fileName}|` }),
-          t.templateElement({ raw: '' })
-        ], [
-          t.callExpression(t.identifier('safeToString'), [
-            t.memberExpression(t.identifier('req'), t.identifier('body'))
-          ])
-        ])]
-      )
-    );
-
-    if (t.isFunctionExpression(handler)) {
-      if (!handler.body || !t.isBlockStatement(handler.body)) return handler;
-
-      const newBody = t.blockStatement([
-        endpointLog,
-        ...handler.body.body
-      ]);
-
-      return t.functionExpression(
-        handler.id,
-        handler.params,
-        newBody,
-        handler.generator,
-        handler.async
-      );
-    } else if (t.isArrowFunctionExpression(handler)) {
-      let newBody;
-
-      if (t.isBlockStatement(handler.body)) {
-        newBody = t.blockStatement([
-          endpointLog,
-          ...handler.body.body
-        ]);
-      } else {
-        // Arrow function with expression body
-        const returnStatement = t.returnStatement(handler.body);
-        newBody = t.blockStatement([
-          endpointLog,
-          returnStatement
-        ]);
-      }
-
-      return t.arrowFunctionExpression(
-        handler.params,
-        newBody,
-        handler.async
-      );
-    }
-
-    return handler;
   }
 }; 
